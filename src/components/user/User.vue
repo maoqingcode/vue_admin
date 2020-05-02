a77f4ed18b1d26442744c424c21cb95ac29a320f
maoqingcode <maoqingcode@163.com>
2020-05-02 08:26:00 +0800

    User.vue

--- src/components/user/User.vue
+++ src/components/user/User.vue
@@ -449,7 +449,7 @@ export default {
       }
 
       this.$message.success('更新角色成功！')
-      this.getUserList()
+      this.queryUserList()
       this.setRoleDialogVisible = false
     },
     // 监听分配角色对话框的关闭事件
