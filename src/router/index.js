229ea1d35ead7768512309e6efc159823b780b73
maoqingcode <maoqingcode@163.com>
2020-05-06 11:10:12 +0800

    finish report

--- src/router/index.js
+++ src/router/index.js
@@ -11,6 +11,7 @@ import Params from '../components/goods/Params.vue'
 import Goods from '../components/goods/Goods.vue'
 import AddGoods from '../components/goods/Add.vue'
 import Order from '../components/order/Order.vue'
+import Report from '../components/report/Report.vue'
 Vue.use(VueRouter)
 
 const routes = [
@@ -29,7 +30,8 @@ const routes = [
       { path: '/params', component: Params },
       { path: '/goods', component: Goods },
       { path: '/goods/add', component: AddGoods },
-      { path: '/orders', component: Order }]
+      { path: '/orders', component: Order },
+      { path: '/reports', component: Report }]
   }
 ]
 const router = new VueRouter({
